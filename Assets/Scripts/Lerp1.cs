3c9c1073e50049b713706d5479760d05ad10665e
nolawetemketem <ntemkete@umd.edu>
2026-02-13 19:38:19 -0500

    fireworks

--- Assets/Scripts/Lerp1.cs
+++ Assets/Scripts/Lerp1.cs
@@ -10,10 +10,11 @@ public class Lerp1 : MonoBehaviour
 {
     [Header("Lerp")]
     public GameObject parent;
-    GameObject[] spheres;
-    static int numSphere = 1000; 
+    GameObject[][] spheres;
+    static int numSphere = 250;
+    static int numHearts = 6;
     float time = 0f;
-    Vector3[] startPosition, endPosition;
+    Vector3[][] startPosition, endPosition;
 
 
    [Header("Rotation")]
@@ -36,81 +37,88 @@ public class Lerp1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        // Assign proper types and sizes to the variables.
-        spheres = new GameObject[numSphere];
-        startPosition = new Vector3[numSphere]; 
-        endPosition = new Vector3[numSphere]; 
+        // Initialize jagged arrays - each heart has numSphere * (heart + 1) spheres
+        spheres = new GameObject[numHearts][];
+        startPosition = new Vector3[numHearts][]; 
+        endPosition = new Vector3[numHearts][]; 
         
-        // Define target positions. Start = random, End = heart 
-        for (int i = 0; i < numSphere; i++){
-            // Random start positions
-            float r = 15f;
-            startPosition[i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));        
-
-            r = 7.5f; // radius of the circle
-            // Circular end position
-            // endPosition[i] = new Vector3(r * Mathf.Sin(i * 2 * Mathf.PI / numSphere), r * Mathf.Cos(i * 2 * Mathf.PI / numSphere));
-            // Heart end position
-
-            float sint = Mathf.Sin(i * 2 * Mathf.PI / numSphere);
-            float cost = Mathf.Cos(i * 2 * Mathf.PI / numSphere);
-            endPosition[i] =
-                new Vector3(r * (Mathf.Sqrt(2) * Mathf.Pow(sint, 3)), 5f + r * (-Mathf.Pow(cost, 3) - Mathf.Pow(cost, 2) + 2 * cost), 20f);
-        }
-        // Let there be spheres..
-        for (int i = 0; i < numSphere; i++){
-            // Draw primitive elements:
-            // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/GameObject.CreatePrimitive.html
-            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
-
-
-            spheres[i].transform.localScale = Vector3.one * 0.1f; // Scale down the spheres
-            spheres[i].transform.parent = parent.transform;
-
-            // Position
-            spheres[i].transform.position = startPosition[i];
-
-            // Color. Get the renderer of the spheres and assign colors.
-            Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
-            // HSV color space: https://en.wikipedia.org/wiki/HSL_and_HSV
-            float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(hue, 1f, 1f); // Full saturation and brightness
-            sphereRenderer.material.color = color;
-
-
-                        // --- Add Trail Renderer ---
-            TrailRenderer trail = spheres[i].AddComponent<TrailRenderer>();
-            trail.time = trailTime;
-            trail.startWidth = 0.08f;
-            trail.endWidth = 0f;
-            trail.minVertexDistance = 0.02f;
-            trail.alignment = LineAlignment.View;
+        // Create multiple hearts with increasing radius and increasing number of spheres
+        for (int heart = 0; heart < numHearts; heart++)
+        {
+            int spheresPerHeart = numSphere * (heart + 1); // Each heart has progressively more spheres
+            spheres[heart] = new GameObject[spheresPerHeart];
+            startPosition[heart] = new Vector3[spheresPerHeart]; 
+            endPosition[heart] = new Vector3[spheresPerHeart];
             
-            // Assign material if provided, otherwise create a default
-            if (trailMaterial != null)
-            {
-                trail.material = trailMaterial;
-            }
-            else
-            {
-                trail.material = new Material(Shader.Find("Sprites/Default"));
-            }
+            float heartRadius = 4f + (heart * 4f); // Increasing radius for each heart
+            
+            // Define target positions for each heart layer
+            for (int i = 0; i < spheresPerHeart; i++){
+                // Random start positions
+                float r = 15f + (heart * 4f);
+                startPosition[heart][i] = new Vector3(r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f), r * Random.Range(-1f, 1f));        
 
-            // Optional: make trail match sphere color
-            Gradient g = new Gradient();
-            g.SetKeys(
-                new GradientColorKey[] {
-                    new GradientColorKey(color, 0f),
-                    new GradientColorKey(color, 1f)
-                },
-                new GradientAlphaKey[] {
-                    new GradientAlphaKey(.5f, 0f),
-                    new GradientAlphaKey(0f, .0f)
+                // Heart end position with increasing radius
+                float sint = Mathf.Sin(i * 2 * Mathf.PI / spheresPerHeart);
+                float cost = Mathf.Cos(i * 2 * Mathf.PI / spheresPerHeart);
+                endPosition[heart][i] =
+                    new Vector3(heartRadius * (Mathf.Sqrt(2) * Mathf.Pow(sint, 3)), heart * 3f + heartRadius * (-Mathf.Pow(cost, 3) - Mathf.Pow(cost, 2) + 2 * cost), 20f);
+            }
+        }
+        
+        // Let there be spheres..
+        for (int heart = 0; heart < numHearts; heart++){
+            int spheresPerHeart = numSphere * (heart + 1);
+            for (int i = 0; i < spheresPerHeart; i++){
+                // Draw primitive elements:
+                // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/GameObject.CreatePrimitive.html
+                spheres[heart][i] = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+
+                spheres[heart][i].transform.localScale = Vector3.one * 0.1f; // Scale down the spheres
+                spheres[heart][i].transform.parent = parent.transform;
+
+                // Position
+                spheres[heart][i].transform.position = startPosition[heart][i];
+
+                // Color. Get the renderer of the spheres and assign colors.
+                Renderer sphereRenderer = spheres[heart][i].GetComponent<Renderer>();
+                // HSV color space: https://en.wikipedia.org/wiki/HSL_and_HSV
+                float hue = (float)i / spheresPerHeart; // Hue cycles through 0 to 1
+                Color color = Color.HSVToRGB(hue, 1f, 1f); // Full saturation and brightness
+                sphereRenderer.material.color = color;
+
+                // --- Add Trail Renderer ---
+                TrailRenderer trail = spheres[heart][i].AddComponent<TrailRenderer>();
+                trail.time = trailTime;
+                trail.startWidth = 0.08f;
+                trail.endWidth = 0f;
+                trail.minVertexDistance = 0.02f;
+                trail.alignment = LineAlignment.View;
+                
+                // Assign material if provided, otherwise create a default
+                if (trailMaterial != null)
+                {
+                    trail.material = trailMaterial;
                 }
-            );
-            trail.colorGradient = g;
+                else
+                {
+                    trail.material = new Material(Shader.Find("Sprites/Default"));
+                }
 
+                // Optional: make trail match sphere color
+                Gradient g = new Gradient();
+                g.SetKeys(
+                    new GradientColorKey[] {
+                        new GradientColorKey(color, 0f),
+                        new GradientColorKey(color, 1f)
+                    },
+                    new GradientAlphaKey[] {
+                        new GradientAlphaKey(.5f, 0f),
+                        new GradientAlphaKey(0f, .0f)
+                    }
+                );
+                trail.colorGradient = g;
+            }
         }
     }
 
@@ -134,7 +142,7 @@ public class Lerp1 : MonoBehaviour
             FormHeart();
 
             GradientAlphaKey[] a = new GradientAlphaKey[] {
-                    new GradientAlphaKey(.4f, 0f),
+                    new GradientAlphaKey(.3f, 0f),
                     new GradientAlphaKey(0f, .0f)
                 };
             UpdateSpheresAndTrailColor(a,time);
@@ -145,22 +153,24 @@ public class Lerp1 : MonoBehaviour
     {
         // lerpFraction variable defines the point between startPosition and endPosition (0~1)
         // let it oscillate over time using sin function
-        float lerpFraction = Mathf.Sin((time/1.5f) - 0.5f) * 0.5f + 0.5f;
 
-
-        for (int i = 0; i < numSphere; i++){
-            // Lerp : Linearly interpolates between two points.
-            // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
-            // Vector3.Lerp(startPosition, endPosition, lerpFraction)
-            
-            // Lerp logic. Update position
-            spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
-            // For now, start positions and end positions are fixed. But what if you change it over time?
-            // startPosition[i]; endPosition[i];
+        for (int heart = 0; heart < numHearts; heart++){
+            int spheresPerHeart = numSphere * (heart + 1);
+            float lerpFraction = Mathf.Sin(((time/((.5f)*(heart) + 1f)/1.5f)) - 0.5f) * 0.5f + 0.5f;
+            for (int i = 0; i < spheresPerHeart; i++){
+                // Lerp : Linearly interpolates between two points.
+                // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
+                // Vector3.Lerp(startPosition, endPosition, lerpFraction)
+                
+                // Lerp logic. Update position
+                spheres[heart][i].transform.position = Vector3.Lerp(startPosition[heart][i], endPosition[heart][i], lerpFraction);
+                // For now, start positions and end positions are fixed. But what if you change it over time?
+                // startPosition[heart, i]; endPosition[heart, i];
+            }
         }
-        if (lerpFraction >= .999f) {
-            heartFormed = true;
-        } 
+        // if (lerpFraction >= .999f) {
+        //     heartFormed = true;
+        // } 
     }
 
     void Rotate()
@@ -175,27 +185,31 @@ public class Lerp1 : MonoBehaviour
     void UpdateSpheresAndTrailColor(GradientAlphaKey[] a, float currentTime)
     {
         // Update sphere and trail colors in unison - same color, same time
-        for (int i = 0; i < numSphere; i++)
+        for (int heart = 0; heart < numHearts; heart++)
         {
-            // Calculate the color once
-            float hue = (float)i / numSphere;
-            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(currentTime)), Mathf.Cos(currentTime), 2f + Mathf.Cos(currentTime));
-            
-            // Update sphere with this color
-            Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
-            sphereRenderer.material.color = color;
-
-            // Update trail with the exact same color
-            TrailRenderer trail = spheres[i].GetComponent<TrailRenderer>();
-            if (trail != null)
+            int spheresPerHeart = numSphere * (heart + 1);
+            for (int i = 0; i < spheresPerHeart; i++)
             {
-               Gradient g = new Gradient();
-                g.SetKeys(
-                    new GradientColorKey[] {
-                        new GradientColorKey(color, 0f),
-                        new GradientColorKey(color, 1f)
-                    },a);
-                trail.colorGradient = g;
+                // Calculate the color once
+                float hue = (float)i / spheresPerHeart;
+                Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(currentTime)), Mathf.Cos(currentTime), 2f + Mathf.Cos(currentTime));
+                
+                // Update sphere with this color
+                Renderer sphereRenderer = spheres[heart][i].GetComponent<Renderer>();
+                sphereRenderer.material.color = color;
+
+                // Update trail with the exact same color
+                TrailRenderer trail = spheres[heart][i].GetComponent<TrailRenderer>();
+                if (trail != null)
+                {
+                   Gradient g = new Gradient();
+                    g.SetKeys(
+                        new GradientColorKey[] {
+                            new GradientColorKey(color, 0f),
+                            new GradientColorKey(color, 1f)
+                        },a);
+                    trail.colorGradient = g;
+                }
             }
         }
     }
